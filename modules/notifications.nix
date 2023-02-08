3173b63baf0fbe53befe356e1e2f2cb0978551d7
friedow <christian@friedow.com>
2023-02-08 13:25:51 +0100

    feat: style notifications

--- modules/notifications.nix
+++ modules/notifications.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 let
   colors = import ../config/colors.nix;
   fonts = import ../config/fonts.nix;
@@ -10,9 +10,13 @@ in {
                 origin = "bottom-right";
                 offset = "15x20";
                 width = "400";
+                height = "120";
                 frame_width = "0";
-                padding = "15";
-                horizontal_padding = "20";
+                padding = "10";
+                horizontal_padding = "10";
+                text_icon_padding = "10";
+
+                vertical_alignment = "top";
                 
                 gap_size = "15";
 
@@ -21,6 +25,13 @@ in {
 
                 timeout = 10;
                 font = "JetBrainsMono Nerd Font 10";
+
+                show_indicators = "no";
+                min_icon_size = 100;
+                max_icon_size = 100;
+
+                browser = "${pkgs.xdg-utils}/bin/xdg-open";
+                mouse_left_click = "do_action";
             };
 
             urgency_normal = {
