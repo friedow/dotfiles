96ca733efde176aef2c242b2660eea6bc69ba888
friedow <christian@friedow.com>
2023-02-15 07:50:32 +0100

    fix: correct path output in rofi git repos systemd action

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -67,7 +67,7 @@ in {
             #!${pkgs.stdenv.shell}
             set -euo pipefail
 
-            find ~ -not -path '*/.*/.git' -type d -name '.git' | sed 's/^\(.*\/\(.*\)\)\/.git$/\2 \1/' | xargs printf '%s\0info\x1f%s\n' > "~/.cache/rofi-git-repositories.txt"
+            find ~ -not -path '*/.*/.git' -type d -name '.git' | sed 's/^\(.*\/\(.*\)\)\/.git$/\2 \1/' | xargs printf '%s\0info\x1f%s\n' > ~/.cache/rofi-git-repositories.txt
         '';
         serviceConfig = {
             Type = "oneshot";
