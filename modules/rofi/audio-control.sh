8a2197792f958a519e50acef50d970e6e8b6c465
friedow <christian@friedow.com>
2023-02-17 08:32:38 +0100

    wip: implement get default sink

--- modules/rofi/audio-control.sh
+++ modules/rofi/audio-control.sh
@@ -6,8 +6,23 @@
 # install all dependencies
 # nix shell nixpkgs#pulseaudio nixpkgs#jq --command "zsh"
 
+function getSinks() {
+    pactl -f json list sinks | jq 'map({index, name, description})'
+}
+
+function getDefaultSink() {
+    local defaultSinkName="$(pactl get-default-sink)"
+    getSinks | jq "map(select(.name == \"$defaultSinkName\"))"
+}
+
+function getSinkDescriptions() {
+    local sinks="$1"
+    echo "$sinks" | jq -r 'map([.description] | @sh) | .[]'
+}
+
 function listEntries() {
-    pactl -f json list sinks | jq -r 'map([.description, .index] | @sh) | .[]'
+    getSinkDescriptions "$(getDefaultSink)"
+    # pactl -f json list sinks | jq -r 'map([.description, .index] | @sh) | .[]'
 }
 
 function executeEntryAction() {
@@ -15,12 +30,12 @@ function executeEntryAction() {
 }
 
 function main() {
-    local selectedEntry=$1
+    local selectedEntry="$1"
 
     if [[ -z $selectedEntry ]]; then
         listEntries
     else
-        executeEntryAction $selectedEntry
+        executeEntryAction "$selectedEntry"
     fi
 }
 
