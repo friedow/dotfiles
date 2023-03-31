d81fc9bb6828ad9cd0f7cc6bff35ee4a38198877
Christian Friedow <christian@friedow.com>
2023-03-31 20:05:28 +0200

    chore: remove dotbase hosts

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,19 +3,7 @@
   users.users.christian.extraGroups = [ "networkmanager" ];
 
   networking = {
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     useDHCP = lib.mkDefault true;
-
-    hosts = {
-      "127.0.0.1" = [
-        "dotbase.local"
-        "monitoring.dotbase.local"
-        "link.dotbase.local"
-      ];
-    };
 
     networkmanager = {
       enable = true;
