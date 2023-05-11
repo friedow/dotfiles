2e8c332e835b053d8073133f5d219e17b69244cb
Christian Friedow <christian@friedow.com>
2023-05-11 10:59:45 +0200

    fix: use stable nushell package

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,21 +1,6 @@
-{ pkgs, inputs, ... }: 
-let 
-  # TODO: import unstable packages globally
-  pkgs-unstable = (import inputs.nixpkgs-unstable) {
-    system = "x86_64-linux";
-    config.allowUnfree = true;
-  };
-in {
-  systemd.user.services.pueued = {
-    wantedBy = ["default.target"];
-    script = ''
-      ${pkgs.pueue}/bin/pueued
-    '';
-  };
+{ pkgs, ... }: {
   home-manager.users.christian.programs.nushell = {
     enable = true;
-    # TODO: use unstable package
-    package = pkgs-unstable.nushell;
 
     extraConfig = ''
       let-env PROMPT_INDICATOR = "> "
