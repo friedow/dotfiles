c47e394583ad4d3e179bd680d37a6435a324714a
Christian Friedow <christian@friedow.com>
2023-05-13 14:03:19 +0200

    feat: add ssh server config

--- modules/ssh-server.nix
+++ modules/ssh-server.nix
@@ -1,3 +1,11 @@
 { ... }: {
-  # TODO: add ssh config from server
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+  };
+
+  users.users.christian.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5Wu0BzLVlygGur8DMjzIV8EeRS8X6MMY2CjYx7g9XD"
+  ];
 }
