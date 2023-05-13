4b1f79dffd5c1448ad8bbffd9c7bb70ccf13d9b7
Christian Friedow <christian@friedow.com>
2023-05-13 13:34:35 +0200

    feat: add traefik config

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -1,14 +1,31 @@
 { ... }: {
   virtualisation.arion.projects = {
-    # TODO: add traefik service
-
     reverse-proxy = {
       project.name = "reverse-proxy";
       services = {
         reverse-proxy.service = {
-          image = "";
-          restart = "unless-stopped";
-          environment = { POSTGRESS_PASSWORD = "password"; };    
+          image = "traefik:latest";
+          restart = "always";
+          command = [
+            "--providers.docker=true"
+            "--providers.docker.exposedbydefault=false"
+            "--entrypoints.web.address=:80"
+            "--entrypoints.web.http.redirections.entrypoint.to=websecure"
+            "--entrypoints.websecure.address=:443"
+            "--certificatesresolvers.le.acme.httpchallenge=true"
+            "--certificatesresolvers.le.acme.httpchallenge.entrypoint=web"
+            "--certificatesresolvers.le.acme.storage=/letsencrypt/acme.json"
+            # Change the below to match your email address
+            "--certificatesresolvers.le.acme.email=example@example.com"
+          ];
+          ports = [
+            "443:443"
+            "80:80"
+          ];
+          volumes = [
+            "./letsencrypt:/letsencrypt"
+            "/var/run/docker.sock:/var/run/docker.sock:ro"
+          ];
         };
       };
     };
