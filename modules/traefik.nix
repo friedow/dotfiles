e755b695d4e67ba82d61fe4d7784c7a02c7051d0
Christian Friedow <christian@friedow.com>
2023-05-13 11:26:10 +0200

    feat: add arion project skeleton

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -1,5 +1,16 @@
 { ... }: {
   virtualisation.arion.projects = {
     # TODO: add traefik service
+
+    reverse-proxy = {
+      project.name = "reverse-proxy";
+      services = {
+        reverse-proxy.service = {
+          image = "";
+          restart = "unless-stopped";
+          environment = { POSTGRESS_PASSWORD = "password"; };    
+        };
+      };
+    };
   };
 }
