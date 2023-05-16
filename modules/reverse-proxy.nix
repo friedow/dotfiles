9fa31bfc9c4d59f67ccfd1fea295ec28d650c95b
Christian Friedow <christian@friedow.com>
2023-05-16 11:21:04 +0200

    fix: arion config keys

--- modules/reverse-proxy.nix
+++ modules/reverse-proxy.nix
@@ -1,32 +1,30 @@
 { ... }: {
-  virtualisation.arion.projects = {
-    reverse-proxy = {
-      project.name = "reverse-proxy";
-      services = {
-        reverse-proxy.service = {
-          image = "traefik:latest";
-          restart = "always";
-          command = [
-            "--providers.docker=true"
-            "--providers.docker.exposedbydefault=false"
-            "--entrypoints.web.address=:80"
-            "--entrypoints.web.http.redirections.entrypoint.to=websecure"
-            "--entrypoints.websecure.address=:443"
-            "--certificatesresolvers.le.acme.httpchallenge=true"
-            "--certificatesresolvers.le.acme.httpchallenge.entrypoint=web"
-            "--certificatesresolvers.le.acme.storage=/letsencrypt/acme.json"
-            # Change the below to match your email address
-            "--certificatesresolvers.le.acme.email=example@example.com"
-          ];
-          ports = [
-            "443:443"
-            "80:80"
-          ];
-          volumes = [
-            "./letsencrypt:/letsencrypt"
-            "/var/run/docker.sock:/var/run/docker.sock:ro"
-          ];
-        };
+  virtualisation.arion.projects.reverse-proxy.settings = {
+    project.name = "reverse-proxy";
+    services = {
+      reverse-proxy.service = {
+        image = "traefik:latest";
+        restart = "always";
+        command = [
+          "--providers.docker=true"
+          "--providers.docker.exposedbydefault=false"
+          "--entrypoints.web.address=:80"
+          "--entrypoints.web.http.redirections.entrypoint.to=websecure"
+          "--entrypoints.websecure.address=:443"
+          "--certificatesresolvers.le.acme.httpchallenge=true"
+          "--certificatesresolvers.le.acme.httpchallenge.entrypoint=web"
+          "--certificatesresolvers.le.acme.storage=/letsencrypt/acme.json"
+          # Change the below to match your email address
+          "--certificatesresolvers.le.acme.email=example@example.com"
+        ];
+        ports = [
+          "443:443"
+          "80:80"
+        ];
+        volumes = [
+          "./letsencrypt:/letsencrypt"
+          "/var/run/docker.sock:/var/run/docker.sock:ro"
+        ];
       };
     };
   };
