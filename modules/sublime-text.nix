144425e4b282d47fbbf5d98027267045f96bd355
Christian Friedow <christian@friedow.com>
2023-06-27 13:22:28 +0200

    fix: sublime text configs

--- modules/sublime-text.nix
+++ modules/sublime-text.nix
@@ -13,6 +13,19 @@
 
     file.".config/sublime-text/Packages/User/Preferences.sublime-settings" = {
       text = builtins.toJSON {
+        "theme" = "ayu-light.sublime-theme";
+        "color_scheme" = "Packages/ayu/ayu-light.sublime-color-scheme";
+        "save_on_focus_lost" = true;
+        "trim_trailing_white_space_on_save" = "all";
+        "shift_tab_unindent" = true;
+        "tab_size" = 2;
+        "translate_tabs_to_spaces" = true;
+        "hot_exit" = "disabled";
+      };
+    };
+
+    file.".config/sublime-text/Packages/User/Package Control.sublime-settings" = {
+      text = builtins.toJSON {
         "bootstrapped" = true;
         "in_process_packages" = [];
         "installed_packages" = [
@@ -29,19 +42,6 @@
           "Nix"
           "Nushell"
         ];
-      };
-    };
-
-    file.".config/sublime-text/Packages/User/Package Control.sublime-settings" = {
-      text = builtins.toJSON {
-        "theme" = "ayu-light.sublime-theme";
-        "color_scheme" = "Packages/ayu/ayu-light.sublime-color-scheme";
-        "save_on_focus_lost" = true;
-        "trim_trailing_white_space_on_save" = "all";
-        "shift_tab_unindent" = true;
-        "tab_size" = 2;
-        "translate_tabs_to_spaces" = true;
-        "hot_exit" = "disabled";
       };
     };
   };
