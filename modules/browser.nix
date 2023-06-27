8090bef0c4ae6a4b18b6721b76f504874a2a9d9b
Christian Friedow <christian@friedow.com>
2023-06-27 09:54:44 +0200

    feat: enable remote debugging for brave

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,12 +1,23 @@
-{ pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  # hack to make programs.chromium.commandLineArgs work for brave
+  brave = pkgs.brave.override {
+    commandLineArgs = lib.concatStringsSep " " config.home-manager.users.christian.programs.chromium.commandLineArgs;
+  };
+in {
   home-manager.users.christian.programs.chromium = {
     enable = true;
-    package = pkgs.brave;
+    package = brave;
     extensions = [
       { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1password
       { id = "fihnjjcciajhdojfnbdddfaoknhalnja"; } # i dont care about cookies
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "nhdogjmejiglipccpnnnanhbledajbpd"; } # vue.js devtools
+    ];
+    # This option only works for chromium
+    # TODO: fix this in https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix#L173g
+    commandLineArgs = [
+      "--remote-debugging-port=9222"
     ];
   };
 }
