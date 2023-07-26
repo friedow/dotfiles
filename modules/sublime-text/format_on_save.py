28f3eef027bba88e049bb42e51e14f436731767a
Christian Friedow <christian@friedow.com>
2023-07-26 11:29:24 +0200

    ✨ sublime: add format on save plugin

---
+++ modules/sublime-text/format_on_save.py
@@ -0,0 +1,20 @@
+import sublime_plugin
+from subprocess import call
+
+class FormatOnSave(sublime_plugin.EventListener):
+  def on_post_save_async(self, view):
+    syntax = view.syntax().name
+
+    if syntax == "Nix":
+      process = call(["nix", "run", "nixpkgs#nixfmt", "--", view.file_name()])
+      return
+
+    elif syntax == "Rust":
+      process = call(["nix", "run", "nixpkgs#rustfmt", "--", view.file_name()])
+      return
+
+    elif syntax in ["JavaScript", "TypeScript", "JSON", "Vue", "HTML", "CSS", "Markdown", "JSX", "YAML"]:
+      process = call(["nix", "run", "nixpkgs#nodePackages.prettier", "--", "-w", view.file_name()])
+      return
+
+    print("unhandled syntax: " + syntax)
