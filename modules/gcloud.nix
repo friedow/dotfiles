2844b058badc9f5af1578257a9d74d2719a83465
Christian Friedow <christian@friedow.com>
2023-07-26 11:31:32 +0200

    ➕ dependencies: add gcloud-cli & kubectl

---
+++ modules/gcloud.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  home-manager.users.christian.home.packages = with pkgs; [
+    (google-cloud-sdk.withExtraComponents
+      [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    kubectl
+  ];
+}
