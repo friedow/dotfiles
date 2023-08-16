81a0272142510f1cba735821098129a3ec64d20c
Christian Friedow <christian@friedow.com>
2023-08-16 15:59:37 +0200

    🎨 format hardware configs

--- hardware-configuration/landslide.nix
+++ hardware-configuration/landslide.nix
@@ -3,7 +3,8 @@
 
   # nixpkgs.crossSystem.system = "aarch64-linux";
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "usbhid" "sr_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "virtio_pci" "usbhid" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -35,7 +36,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [ { device = "/dev/disk/by-label/SWAP"; } ];
+  swapDevices = [{ device = "/dev/disk/by-label/SWAP"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
