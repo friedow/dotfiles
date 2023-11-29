172b21c6548e8650433de7681de76415d3a8408d
Christian Friedow <christian@friedow.com>
2023-11-29 18:07:33 +0100

    ✨ add colorscheme for terminal

--- config/colors.nix
+++ config/colors.nix
@@ -5,8 +5,6 @@
     inverted = "#000000";
   };
 
-  text = "#24292f";
-
   highlight = {
     black = "#24292e";
     blue = "#0366d6";
@@ -18,4 +16,35 @@
     white = "#6a737d";
     gray = "#cdc7c2";
   };
+
+  rosewater = "#dc8a78";
+  flamingo = "#dd7878";
+  pink = "#ea76cb";
+  mauve = "#8839ef";
+  red = "#d20f39";
+  maroon = "#e64553";
+  peach = "#fe640b";
+  yellow = "#df8e1d";
+  green = "#40a02b";
+  teal = "#179299";
+  sky = "#04a5e5";
+  sapphire = "#209fb5";
+  blue = "#1e66f5";
+  lavender = "#7287fd";
+
+  text = "#565976";
+  subtext1 = "#666a85";
+  subtext0 = "#787b91";
+
+  overlay2 = "#898c9f";
+  overlay1 = "#9a9dac";
+  overlay0 = "#aaadbb";
+
+  surface2 = "#babec9";
+  surface1 = "#cbced8";
+  surface0 = "#dcdee5";
+
+  crust = "#eceff3";
+  mantle = "#f6f7f9";
+  base = "#ffffff";
 }
--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,7 +1,5 @@
 { ... }:
-let
-  colors = import ../config/colors.nix;
-  fonts = import ../config/fonts.nix;
+let color = import ../config/colors.nix;
 in {
   environment.variables.TERM = "alacritty";
 
@@ -16,25 +14,106 @@ in {
       # UI
       colors = {
         primary = {
-          foreground = colors.text;
-          background = colors.background.primary;
+          background = "${color.base}"; # base
+          foreground = "${color.text}"; # text
+          # Bright and dim foreground colors
+          dim_foreground = "${color.text}"; # text
+          bright_foreground = "${color.text}"; # text
         };
 
+        # Cursor colors
+        cursor = {
+          text = "${color.base}"; # base
+          cursor = "${color.rosewater}"; # rosewater
+        };
+        vi_mode_cursor = {
+          text = "${color.base}"; # base
+          cursor = "${color.lavender}"; # lavender
+        };
+
+        # Search colors
+        search = {
+          matches = {
+            foreground = "${color.base}"; # base
+            background = "${color.subtext0}"; # subtext0
+          };
+          focused_match = {
+            foreground = "${color.base}"; # base
+            background = "${color.green}"; # green
+          };
+          footer_bar = {
+            foreground = "${color.base}"; # base
+            background = "${color.subtext0}"; # subtext0
+          };
+        };
+
+        # Keyboard regex hints
+        hints = {
+          start = {
+            foreground = "${color.base}"; # base
+            background = "${color.yellow}"; # yellow
+          };
+          end = {
+            foreground = "${color.base}"; # base
+            background = "${color.subtext0}"; # subtext0
+          };
+        };
+
+        # Selection colors
+        selection = {
+          text = "${color.base}"; # base
+          background = "${color.rosewater}"; # rosewater
+        };
+
+        # Normal colors
         normal = {
-          black = colors.highlight.black;
-          blue = colors.highlight.blue;
-          cyan = colors.highlight.cyan;
-          green = colors.highlight.green;
-          magenta = colors.highlight.magenta;
-          red = colors.highlight.red;
-          yellow = colors.highlight.yellow;
-          white = colors.highlight.white;
+          black = "${color.subtext1}"; # subtext1
+          red = "${color.red}"; # red
+          green = "${color.green}"; # green
+          yellow = "${color.yellow}"; # yellow
+          blue = "${color.blue}"; # blue
+          magenta = "${color.pink}"; # pink
+          cyan = "${color.teal}"; # teal
+          white = "${color.surface2}"; # surface2
         };
+
+        # Bright colors
+        bright = {
+          black = "${color.subtext0}"; # subtext0
+          red = "${color.red}"; # red
+          green = "${color.green}"; # green
+          yellow = "${color.yellow}"; # yellow
+          blue = "${color.blue}"; # blue
+          magenta = "${color.pink}"; # pink
+          cyan = "${color.teal}"; # teal
+          white = "${color.surface1}"; # surface1
+        };
+
+        # Dim colors
+        dim = {
+          black = "${color.subtext1}"; # subtext1
+          red = "${color.red}"; # red
+          green = "${color.green}"; # green
+          yellow = "${color.yellow}"; # yellow
+          blue = "${color.blue}"; # blue
+          magenta = "${color.pink}"; # pink
+          cyan = "${color.teal}"; # teal
+          white = "${color.surface2}"; # surface2
+        };
+
+        indexed_colors = [
+          {
+            index = 16;
+            color = "${color.peach}";
+          }
+          {
+            index = 17;
+            color = "${color.rosewater}";
+          }
+        ];
       };
 
-      cursor.style = {
-        shape = "Beam";
-      };
+      cursor.style = { shape = "Beam"; };
 
       font = { size = 9; };
 
