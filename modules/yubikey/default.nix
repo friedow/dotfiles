217a8c09b67066cf2d40b9d3f4ff182a2a9ed198
Christian Friedow <christian@friedow.com>
2023-12-05 22:35:09 +0100

    📝 comment how to monitor udev properties

--- modules/yubikey/default.nix
+++ modules/yubikey/default.nix
@@ -5,6 +5,7 @@
     polkit-1.u2fAuth = true;
   };
 
+  # use `udevadm monitor --property` to get the ENV values
   services.udev.extraRules = ''
     ACTION=="remove", ENV{PRODUCT}=="1050/402/556", ENV{INTERFACE}=="3/0/0", RUN+="${pkgs.fish}/bin/fish -c 'WAYLAND_DISPLAY=wayland-1 XDG_RUNTIME_DIR=/run/user/1001 sudo -u christian -E ${config.lockPackage}/bin/lock &'"
   '';
