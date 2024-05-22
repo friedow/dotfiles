e02bdf735efed70d6b7f1d2f5b23916a7f314ef4
Christian Friedow <christian@friedow.com>
2024-05-22 10:25:01 +0200

    🐛 fix networkmanager wait online service blocking rebuild switch

--- modules/networking.nix
+++ modules/networking.nix
@@ -9,4 +9,12 @@
   };
 
   programs.nm-applet.enable = true;
+
+  # see https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online = {
+    serviceConfig = {
+      ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
+    };
+  };
+
 }
