9ee4e708908c4b1a81119ecb16408db1dbb4c1d1
Christian Friedow <christian@friedow.com>
2024-07-08 15:44:11 +0200

    ✨ enable pubsub emulator in gcloud cli

--- modules/gcloud.nix
+++ modules/gcloud.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
   home-manager.users.christian.home.packages = with pkgs; [
-    (google-cloud-sdk.withExtraComponents
-      [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    (google-cloud-sdk.withExtraComponents [
+      google-cloud-sdk.components.gke-gcloud-auth-plugin
+      google-cloud-sdk.components.pubsub-emulator
+    ])
     kubectl
   ];
 }
