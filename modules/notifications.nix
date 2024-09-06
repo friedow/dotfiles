5c650f8f48812853fd7650a23ebbcc630c25d346
Christian Friedow <christian@friedow.com>
2024-09-06 19:47:26 +0200

    💄 tune dunst style

--- modules/notifications.nix
+++ modules/notifications.nix
@@ -5,22 +5,25 @@
       global = {
         origin = "bottom-right";
         offset = "15x20";
-        width = "400";
-        height = "120";
+        width = "300";
+        height = "95";
         frame_width = "0";
         padding = "10";
         horizontal_padding = "10";
-        text_icon_padding = "10";
+        text_icon_padding = "15";
 
         vertical_alignment = "top";
 
         gap_size = "15";
+        corner_radius = "3";
+        progress_bar_corner_radius = "3";
+        icon_corner_radius = "3";
 
         timeout = 10;
 
         show_indicators = "no";
-        min_icon_size = 100;
-        max_icon_size = 100;
+        min_icon_size = 75;
+        max_icon_size = 75;
 
         browser = "${pkgs.xdg-utils}/bin/xdg-open";
         mouse_left_click = "do_action";
