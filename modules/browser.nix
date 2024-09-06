ef2098c7d9081956f21a3e6c3a5d9b35365faf54
Christian Friedow <christian@friedow.com>
2024-09-06 19:17:38 +0200

    🔥 remove unnecessary debugging port in browser

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,23 +1,12 @@
-{ config, lib, pkgs, ... }:
-let
-  # hack to make programs.chromium.commandLineArgs work for brave
-  brave = pkgs.brave.override {
-    commandLineArgs = lib.concatStringsSep " " config.home-manager.users.christian.programs.chromium.commandLineArgs;
-  };
-in {
+{ pkgs, ... }: {
   home-manager.users.christian.programs.chromium = {
     enable = true;
-    package = brave;
+    package = pkgs.brave;
     extensions = [
       { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1password
       { id = "fihnjjcciajhdojfnbdddfaoknhalnja"; } # i dont care about cookies
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "nhdogjmejiglipccpnnnanhbledajbpd"; } # vue.js devtools
-    ];
-    # This option only works for chromium
-    # TODO: fix this in https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix#L173
-    commandLineArgs = [
-      "--remote-debugging-port=9222"
     ];
   };
 }
