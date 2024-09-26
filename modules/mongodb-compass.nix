9da94f950df383082c58dda90b7fbbc11fcc108c
Christian Friedow <christian@friedow.com>
2024-09-26 15:53:41 +0200

    🐛 set wayland flags for mongodb-compass

--- modules/mongodb-compass.nix
+++ modules/mongodb-compass.nix
@@ -1,4 +1,11 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 {
-  home-manager.users.christian.home.packages = [ pkgs.mongodb-compass ];
+  # TODO: update unstable as soon as https://github.com/NixOS/nixpkgs/pull/344661 is merged
+  home-manager.users.christian.home.packages = [ pkgs-unstable.mongodb-compass ];
+  home-manager.users.christian.xdg.desktopEntries."mongodb-compass" = {
+    name = "MongoDB Compass";
+    genericName = "Database Explorer";
+    exec = "mongodb-compass --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --ignore-additional-command-line-flags";
+    terminal = false;
+  };
 }
