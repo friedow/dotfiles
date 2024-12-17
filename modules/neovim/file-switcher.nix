61f8f10164b4203716c16cf49c4763a5ef80c6f8
Christian Friedow <christian@friedow.com>
2024-12-17 21:33:16 +0100

    ✨ use telescope as harpoon ui

--- modules/neovim/file-switcher.nix
+++ modules/neovim/file-switcher.nix
@@ -11,7 +11,20 @@
       {
         action.__raw = ''
           function()
-            require("harpoon.ui"):toggle_quick_menu(require("harpoon"):list())
+            local file_paths = {}
+            for _, item in ipairs(require("harpoon"):list().items) do
+                table.insert(file_paths, item.value)
+            end
+
+            local conf = require("telescope.config").values
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
           end
         '';
         key = "<leader>hh";
