356bfae7f2391ef428264d4f72911742807c16bd
Christian Friedow <christian@friedow.com>
2025-01-07 19:31:36 +0100

    ✨ add gitgraph

--- modules/neovim/source-control.nix
+++ modules/neovim/source-control.nix
@@ -1,12 +1,32 @@
 { pkgs-unstable, ... }:
+let
+  # git@github.com:isakbm/gitgraph.nvim.git
+  gitgraph = pkgs-unstable.vimUtils.buildVimPlugin {
+    name = "gitgraph-nvim";
+    src = pkgs-unstable.fetchFromGitHub {
+      owner = "isakbm";
+      repo = "gitgraph.nvim";
+      rev = "01e466b32c346a165135dd47d42f1244eca06572";
+      hash = "sha256-d55IRrOhK5tSLo2boSuMhDbkerqij5AHgNDkwtGadyI=";
+    };
+  };
+in
 {
-
   home-manager.users.christian.programs.nixvim = {
     keymaps = [
       {
-        action = "<cmd>Flog -format=%s%d -all -order=date -open-cmd=edit<CR>";
+        action.__raw = ''
+          function()
+            require('gitgraph').draw({}, { all = true, max_count = 5000 })
+          end
+        '';
         key = "<C-->";
-        mode = "n";
+        mode = [
+          "n"
+          "t"
+          "v"
+          "i"
+        ];
         options.silent = true;
       }
       {
@@ -21,6 +41,51 @@
       }
     ];
 
+    extraConfigLua = ''
+      require("gitgraph").setup({
+        symbols = {
+          merge_commit = '',
+          commit = '',
+          merge_commit_end = '',
+          commit_end = '',
+
+          -- Advanced symbols
+          GVER = '',
+          GHOR = '',
+          GCLD = '',
+          GCRD = '╭',
+          GCLU = '',
+          GCRU = '',
+          GLRU = '',
+          GLRD = '',
+          GLUD = '',
+          GRUD = '',
+          GFORKU = '',
+          GFORKD = '',
+          GRUDCD = '',
+          GRUDCU = '',
+          GLUDCD = '',
+          GLUDCU = '',
+          GLRDCL = '',
+          GLRDCR = '',
+          GLRUCL = '',
+          GLRUCR = '',
+        },
+        format = {
+          timestamp = '%H:%M:%S %d-%m-%Y',
+          fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+        },
+        hooks = {
+          on_select_commit = function(commit)
+            print('selected commit:', commit.hash)
+          end,
+          on_select_range_commit = function(from, to)
+            print('selected range:', from.hash, to.hash)
+          end,
+        },
+      })
+    '';
+
     plugins.telescope = {
       keymaps = {
         "<leader>gb" = {
@@ -29,17 +94,11 @@
       };
     };
 
-    globals = {
-      flog_enable_extended_chars = true;
-      flog_enable_dynamic_commit_hl = true;
-    };
-
-    extraPlugins = with pkgs-unstable.vimPlugins; [
-      vim-flog
+    extraPlugins = [
+      gitgraph
     ];
 
     plugins = {
-      fugitive.enable = true;
       gitsigns.enable = true;
     };
   };
