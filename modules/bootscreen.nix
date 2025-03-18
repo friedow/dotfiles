5a6905a4cf620925b54cd2136dc78709812655b0
Christian Friedow <christian@friedow.com>
2025-03-18 10:57:46 +0100

    📝 add todo to test boot config

--- modules/bootscreen.nix
+++ modules/bootscreen.nix
@@ -1,7 +1,15 @@
 { ... }:
 {
-  boot.initrd.systemd.enable = true;
+  # TODO: activate & test these settings
+  boot.initrd = {
+    systemd.enable = true;
+    # verbose = false;
+  };
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" ];
+  boot.kernelParams = [
+    "quiet"
+    # "udev.log_level=3"
+  ];
+  # boot.loader.timeout = 0;
   boot.plymouth.enable = true;
 }
