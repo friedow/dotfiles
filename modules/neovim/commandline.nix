06ca4703fc532f0ae25ef0b76e14a53d529f22ce
Christian Friedow <christian@friedow.com>
2025-04-14 15:48:05 +0200

    🔧 configure noice

--- modules/neovim/commandline.nix
+++ modules/neovim/commandline.nix
@@ -1,9 +1,14 @@
 { ... }:
 {
-  home-manager.users.christian.programs.nixvim.plugins.noice = {
-    enable = true;
-    settings = {
-      presets.command_palette = true;
+  home-manager.users.christian.programs.nixvim.plugins = {
+    nui.enable = true;
+    notify.enable = true;
+    noice = {
+      enable = true;
+      settings = {
+        presets.command_palette = true;
+        messages.view_history = "popup";
+      };
     };
   };
 }
