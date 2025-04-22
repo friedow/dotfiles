0050702977df48398a904f94296c96d2fec8bf5e
Christian Friedow <christian@friedow.com>
2025-04-22 18:58:59 +0200

    Revert "✨ add wezterm config to switch tabs"
    This reverts commit 15e221d6fd9018c383bc8f6aa77258dc370b1e76.

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -4,17 +4,6 @@
     enable = true;
     package = pkgs-unstable.wezterm;
     extraConfig = ''
-      wezterm.on('gui-startup', function()
-        local _, first_pane, window = wezterm.mux.spawn_window {}
-        local _, second_pane, _ = window:spawn_tab {}
-        local _, third_pane, _ = window:spawn_tab {}
-        local _, fourth_pane, _ = window:spawn_tab {}
-        local _, fifth_pane, _ = window:spawn_tab {}
-
-        -- third_pane:send_text "top\n"
-        -- '\n' this will execute you shell command
-      end)
-
       return {
         window_close_confirmation = "NeverPrompt",
         enable_tab_bar = false,
@@ -24,33 +13,6 @@
           top = "1cell",
           bottom = "1cell",
         },
-        keys = {
-          {
-            key = '6',
-            mods = 'CTRL',
-            action = wezterm.action.ActivateTab(0),
-          },
-          {
-            key = '7',
-            mods = 'CTRL',
-            action = wezterm.action.ActivateTab(1),
-          },
-          {
-            key = '8',
-            mods = 'CTRL',
-            action = wezterm.action.ActivateTab(2),
-          },
-          {
-            key = '9',
-            mods = 'CTRL',
-            action = wezterm.action.ActivateTab(3),
-          },
-          {
-            key = '0',
-            mods = 'CTRL',
-            action = wezterm.action.ActivateTab(4),
-          },
-        }
       }
     '';
   };
