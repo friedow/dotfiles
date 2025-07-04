7e80f42ffdd2b2a5c7df87127c7a82ab4722bbfd
Christian Friedow <christian@friedow.com>
2025-07-04 13:15:51 +0200

    remove pulseaudio and alsa 32 bit support

--- modules/audio.nix
+++ modules/audio.nix
@@ -4,8 +4,8 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+    # alsa.support32Bit = true;
+    # pulse.enable = true;
   };
   home-manager.users.christian.home.packages = with pkgs; [ pavucontrol ];
 }
