56cb56a8a2e08ba2c0b14d1f4413b556759c1299
Christian Friedow <christian@friedow.com>
2025-07-15 20:37:25 +0200

    feat: add neogit

--- modules/neovim/source-control.nix
+++ modules/neovim/source-control.nix
@@ -4,6 +4,28 @@
     plugins = {
       gitsigns.enable = true;
       diffview.enable = true;
+      neogit = {
+        enable = true;
+
+        settings = {
+          commit_editor.kind = "replace";
+          commit_select_view.kind = "replace";
+          commit_view.kind = "replace";
+          description_editor.kind = "replace";
+          log_view.kind = "replace";
+          merge_editor.kind = "replace";
+          rebase_editor.kind = "replace";
+          reflog_view.kind = "replace";
+          tag_editor.kind = "replace";
+
+          filewatcher.enable = true;
+
+          integrations = {
+            diffview = true;
+            telescope = true;
+          };
+        };
+      };
     };
   };
 }
