4405a09cbe7957df6700fd182c405c45e401b20e
Christian Friedow <christian@friedow.com>
2025-08-31 12:10:41 +0200

    fix(bootstick): depend on base nixos iso config

--- hardware-configuration/bootstick.nix
+++ hardware-configuration/bootstick.nix
@@ -1,21 +1,7 @@
-{ ... }:
+{ modulesPath, ... }:
 {
+  imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-base.nix") ];
   networking.hostName = "bootstick";
   system.stateVersion = "25.05";
   home-manager.users.christian.home.stateVersion = "25.05";
-
-  boot = {
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "nvme"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-    ];
-    kernelModules = [ "kvm-intel" ];
-    loader.systemd-boot.enable = true;
-  };
-
-  powerManagement.cpuFreqGovernor = "powersave";
-  hardware.enableRedistributableFirmware = true;
 }
