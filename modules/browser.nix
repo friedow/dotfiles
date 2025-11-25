dcc7b3bca578b2abf6ce0d24c3ff8bc5f03c6a0e
Christian Friedow <christian@friedow.com>
2025-11-25 20:50:31 +0100

    ✨ add vimium browser extension

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 {
-  home-manager.users.christian.programs.chromium = {
-    enable = true;
-    package = pkgs.brave;
-    extensions = [
-      { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1password
-    ];
-  };
+  home-manager.users.christian = {
+    programs.chromium = {
+      enable = true;
+      package = pkgs.brave;
+      extensions = [
+        { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1password
+        { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # vimium
+      ];
+    };
 
-  home-manager.users.christian.xdg.desktopEntries."brave" = {
-    name = "Brave";
-    genericName = "Browser";
-    exec = "brave --enable-features=UseOzonePlatform --ozone-platform=wayland";
-    terminal = false;
+    xdg.desktopEntries."brave" = {
+      name = "Brave";
+      genericName = "Browser";
+      exec = "brave --enable-features=UseOzonePlatform --ozone-platform=wayland";
+      terminal = false;
+    };
   };
 }
