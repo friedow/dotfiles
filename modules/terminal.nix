953d4196acd03e40f3c92eee3ed240d1b58caf35
Christian Friedow <christian@friedow.com>
2025-12-16 20:08:43 +0100

    feat: add rio terminal

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,16 +1,30 @@
 { pkgs, ... }:
 {
-  home-manager.users.christian.programs.kitty = {
-    enable = true;
+  home-manager.users.christian.programs = {
+    kitty = {
+      enable = true;
 
-    settings = {
-      confirm_os_window_close = 0;
-      cursor_blink_interval = 0;
-      cursor_shape = "beam";
-      window_padding_width = 10;
+      settings = {
+        confirm_os_window_close = 0;
+        cursor_blink_interval = 0;
+        cursor_shape = "beam";
+        window_padding_width = 10;
 
-      "map ctrl+c" = "copy_or_interrupt";
-      "map ctrl+v" = "paste_from_clipboard";
+        "map ctrl+c" = "copy_or_interrupt";
+        "map ctrl+v" = "paste_from_clipboard";
+      };
+    };
+
+    rio = {
+      enable = true;
+      settings = {
+        confirm-before-quit = false;
+        padding-x = 10;
+        padding-y = [
+          10
+          10
+        ];
+      };
     };
   };
 }
