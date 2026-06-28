298c0484d2a0bcbbd68891b9eda6f4e52d495158
Christian Friedow <christian@friedow.com>
2026-06-28 19:42:01 +0200

    ♻️ replace telescope with the snacks.nvim picker

--- modules/neovim/fuzzy-finder.nix
+++ modules/neovim/fuzzy-finder.nix
@@ -7,48 +7,33 @@
     ];
 
     programs.nixvim = {
-      extraConfigLua = ''
-        require('telescope').load_extension('ui-select')
-      '';
-
-      plugins.telescope = {
-        enable = true;
-        extensions = {
-          fzf-native.enable = true;
-          ui-select.enable = true;
-        };
-
-        keymaps = {
-          "<leader>f" = {
-            action = "find_files find_command=rg,--files,--hidden,--glob=!.git";
-          };
-          "<leader>/" = {
-            action = "live_grep";
-          };
-        };
-
-        settings = {
-          defaults = {
-            layout_strategy = "vertical";
-            layout_config = {
-              width = 0.5;
-              prompt_position = "top";
-              mirror = true;
-            };
-            sorting_strategy = "ascending";
-          };
-
-          pickers = {
-            live_grep = {
-              additional_args.__raw = ''
-                function(opts)
-                  return {"--hidden", "--glob=!.git"}
-                end
-              '';
-            };
-          };
-        };
+      plugins.snacks.settings.picker = {
+        enabled = true;
+        layout.preset = "vertical";
       };
+
+      keymaps = [
+        {
+          action.__raw = ''
+            function()
+              Snacks.picker.files({ hidden = true })
+            end
+          '';
+          key = "<leader>f";
+          mode = "n";
+          options.silent = true;
+        }
+        {
+          action.__raw = ''
+            function()
+              Snacks.picker.grep({ hidden = true })
+            end
+          '';
+          key = "<leader>/";
+          mode = "n";
+          options.silent = true;
+        }
+      ];
     };
   };
 }
